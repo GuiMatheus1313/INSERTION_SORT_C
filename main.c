a80e19935218d225645c06c62d51b7a7a755be07
Guilherme Araujo <82453465+GuiMatheus1313@users.noreply.github.com>
2024-03-29 19:28:30 +0000

    Funcionamento INSERTION_SORT

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+//TIRAR DÚVIDA COM PROFESSOR
+
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+  int vetor_desor[] = {40,30,15,5,100,25,65,145}; //desorganizado
+  int fim = 8, p , menor = 0, j, escolhido;
+
+
+  for(int i = 1; i < fim; i++)
+  {
+    escolhido = vetor_desor[i];
+    j = i-1;
+    //Checa se o elemento adjacente da esquerda do escolhido é maior que o próprio
+    while ((j >= 0) && (vetor_desor[j] > escolhido))
+    {
+      //move o elemento da esquerda para a direita 
+      vetor_desor[j+1] = vetor_desor[j];
+      j = j-1;
+    }
+    //Ajusta o elemento escolhido em sua posição correta
+    vetor_desor[j+1] = escolhido;
+  }
+
+  for(int i = 0; i < fim ; i++)
+    printf("[%d]: %d\n", i, vetor_desor[i]);
 }
